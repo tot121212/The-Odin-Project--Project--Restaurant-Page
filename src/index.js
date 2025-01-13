0523206c11c639cd0c473e0ff693f3f094b5d383
tot121212 <totmusicbusiness@gmail.com>
2025-01-13 14:04:50 -0600

    base done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 console.log("Hello world!");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector("header");
+    header.innerHTML += `
+        <div>
+            CS Delicatessen
+        </div>
+    `
+
+    const main = document.createElement("main");
+    document.body.appendChild(main);
+    main.innerHTML = `
+        <div>
+            We pride ourselves on the best quality squirrel meat available. Locally sourced, right here in Gootown!
+        </div>
+    `
+
+    const footer = document.createElement("footer");
+    footer.innerHTML = `
+        <form action="" method="get" id="purchase-form">
+            <label for="purchase-amount">How many squirrels would you like?</label>
+            <input id="purchase-amount" type="text" placeholder="#">
+        </form>
+    `;
+    document.body.appendChild(footer);
+
+    const purchaseForm = document.getElementById("purchase-form");
+    purchaseForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const purchaseAmount = document.getElementById("purchase-amount").value;
+        alert(`Thank you for your purchase of ${purchaseAmount} squirrels!`);
+    });
+});
